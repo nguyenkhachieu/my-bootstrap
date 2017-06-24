39b55e1b98c24040536a42e125f0dae6d5fd14c9
nguyenkhachieu <nguyenkhachieu0508@gmail.com>
2017-06-24 09:44:51 +0700

    update code

---
+++ 4MENDAYDU/js/menucodan.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+	$('.menu-page').hover(function(){
+		//alert('abc');
+		// var abc = $('#menu123').width();
+		// $('.menu-page-menu').css('width',abc);
+	});
+});
---
+++ 4MENDAYDU/js/muahang.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+	$('#themvaojs').click(function(){
+		$('.modalPopup').addClass('ab');
+	});
+	$('.thongbao11').click(function(){
+		$('.modalPopup').removeClass('ab');
+	});
+});
